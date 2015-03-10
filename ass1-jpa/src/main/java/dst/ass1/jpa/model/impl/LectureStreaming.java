c0f60184b0a2c674e3bc2a2caf365dce9eb74775
Daniel Shatkin <Daniel.Shatkin@gmail.com>
2015-03-10 11:32:15 +0100

    implementations

---
+++ ass1-jpa/src/main/java/dst/ass1/jpa/model/impl/LectureStreaming.java
@@ -0,0 +1,92 @@
+package dst.ass1.jpa.model.impl;
+
+import dst.ass1.jpa.model.IClassroom;
+import dst.ass1.jpa.model.ILecture;
+import dst.ass1.jpa.model.ILectureStreaming;
+import dst.ass1.jpa.model.LectureStatus;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
+
+public class LectureStreaming implements ILectureStreaming {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private Date start;
+    private  Date end;
+    private LectureStatus lectureStatus;
+
+    @ManyToMany
+    private List<IClassroom> classrooms;
+
+    @OneToOne
+    private  ILecture lecture;
+
+    @Override
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public Date getStart() {
+        return start;
+    }
+
+    @Override
+    public void setStart(Date start) {
+            this.start = start;
+    }
+
+    @Override
+    public Date getEnd() {
+        return end;
+    }
+
+    @Override
+    public void setEnd(Date end) {
+        this.end = end;
+    }
+
+    @Override
+    public LectureStatus getStatus() {
+        return lectureStatus;
+    }
+
+    @Override
+    public void setStatus(LectureStatus status) {
+        this.lectureStatus = status;
+    }
+
+    @Override
+    public List<IClassroom> getClassrooms() {
+        return null;
+    }
+
+    @Override
+    public void setClassrooms(List<IClassroom> list) {
+        this.classrooms = list;
+    }
+
+    @Override
+    public void addClassroom(IClassroom classroom) {
+        this.classrooms.add(classroom);
+    }
+
+    @Override
+    public ILecture getLecture() {
+        return lecture;
+    }
+
+    @Override
+    public void setLecture(ILecture lecture) {
+        this.lecture = lecture;
+    }
+}
---
+++ ass1-jpa/src/main/java/dst/ass1/jpa/model/impl/Metadata.java
@@ -0,0 +1,59 @@
+package dst.ass1.jpa.model.impl;
+
+import dst.ass1.jpa.model.IMetadata;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "metadata")
+public class Metadata implements IMetadata {
+
+
+    public Metadata(String course, List<String> settings) {
+        this.course = course;
+        this.settings = settings;
+    }
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String course;
+    private List<String> settings;
+
+    @Override
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String getCourse() {
+        return course;
+    }
+
+    @Override
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    @Override
+    public List<String> getSettings() {
+        return settings;
+    }
+
+    @Override
+    public void setSettings(List<String> settings) {
+        this.settings = settings;
+    }
+
+    @Override
+    public void addSetting(String setting) {
+        settings.add(setting);
+    }
+}
